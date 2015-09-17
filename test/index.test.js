3789fa8ca0dc022f67d65bb49f85bfaf5bab8caa
Dmitry Demyankov <dmitry.demyankov@gmail.com>
2015-09-17 10:41:34 +0200

    Disabled validateLineBreaks rule due to differences on Windows and other platforms

--- test/index.test.js
+++ test/index.test.js
@@ -1,5 +1,6 @@
 import { expect } from 'chai';
 import Checker from 'jscs/lib/checker';
+import path from 'path';
 
 describe('jscs-config-nordnet', () => {
   let checker;
@@ -12,4 +13,16 @@ describe('jscs-config-nordnet', () => {
   });
 
   it('should define nordnet preset', () => expect(checker._configuration._presets.nordnet).to.be.ok);
+
+  describe('validateLineBreaks', () => {
+    it('should not validate line breaks', function() {
+      checker = new Checker();
+        checker.configure({
+        plugins: ['./lib/index.js']
+      });
+      return checker.checkFile(path.resolve('./test/fixtures/validate-line-breaks.js')).then(function(errors) {
+        expect(errors.getErrorList().length).to.equal(0);
+      });
+    });
+  });
 });
